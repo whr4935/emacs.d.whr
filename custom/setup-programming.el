8c7858ab8717918115e7165aa4c69b133e3b649d
harry wang <wanghourenrd@163.com>
2016-07-05 00:24:43 +0800

    change c++ comment to //

--- custom/setup-programming.el
+++ custom/setup-programming.el
@@ -20,6 +20,12 @@
                         (other . "gnu"))
       c-basic-offset 4)
 
+
+(add-hook 'c++-mode-hook (lambda ()
+                           (setq comment-start "//"
+                                 comment-end "")))
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; GROUP: Programming -> Tools -> Gdb ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
